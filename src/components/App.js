6d97c1aad70e80088183005ebba37b270117ccca
Mike Chi <mikechi@MacBook-Pro-4.local>
2019-06-27 07:05:21 -0400

    completed module

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,19 @@ import { selectSong } from '../actions';
 
 // Component
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
     return (
-        <div>
-            <SongList />
+        <div className="app ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
+            </div>
         </div>
     );
 };
